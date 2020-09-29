7c8d33d388fff8c2e43f4a18bcf9ba65faeda8fa
Gerardo Gómez-Caminero Ortega <gerardo.gomez@factorlibre.com>
2020-09-29 13:26:24 +0200

    Add user agent

--- magento/rest.py
+++ magento/rest.py
@@ -8,12 +8,15 @@ except ImportError:
 
 class Client(object):
 
-    def __init__(self, url, token, verify_ssl=True):
+    def __init__(
+            self, url, token, verify_ssl=True, useragent=None):
         self._url = url
         self._token = token
         self._verify_ssl = verify_ssl
+        self.useragent = useragent
 
-    def call(self, resource_path, arguments, http_method=None, storeview=None):
+    def call(self, resource_path, arguments, http_method=None,
+             storeview=None, headers=None):
         url = '%s/%s' % (self._url, resource_path)
         if storeview:
             # https://github.com/magento/magento2/issues/3864
@@ -21,8 +24,13 @@ class Client(object):
         if http_method is None:
             http_method = 'get'
         function = getattr(requests, http_method)
-        headers = {'Authorization': 'Bearer %s' % self._token}
-        kwargs = {'headers': headers}
+        base_headers = {
+            'Authorization': 'Bearer %s' % self._token,
+            'User-Agent': self.useragent,
+        }
+        if headers:
+            base_headers.update(headers)
+        kwargs = {'headers': base_headers}
         if http_method == 'get':
             kwargs['params'] = arguments
         elif arguments is not None:
